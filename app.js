a78d9564092cd709321e6c2481bb26490cbca385
akt01gali <58081716+akt01gali@users.noreply.github.com>
2021-03-23 23:40:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-  res.send("Hello world");
+  res.send("Subdomain Takeover by akt01gali");
 });
 
 app.listen(PORT, () => {
